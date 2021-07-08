ab8b6b8253674de58b7a0f43666e1f83a99c7bc4
Ian <cameronian0@protonmail.com>
2021-07-08 16:00:07 +0800

    Remove activejob

--- config/initializers/activejob_status.rb
+++ config/initializers/activejob_status.rb
@@ -1,16 +1,16 @@
 
 
-if Rails.cache.is_a?(ActiveSupport::Cache::NullStore)
-  # Use an alternative cache store:
-  # ActiveJob::Status.store = :file_store
-  # ActiveJob::Status.store = :mem_cache_store
-  # ActiveJob::Status.store = :redis_cache_store
-  #ActiveJob::Status.store = :file_store
-    
-  # Avoid using cache store that are not shared by processes (ex: memory_store).
-
-  # The `store=` method doesn't handle multiple arguments like Rails.
-  # If you need to pass argument, you should instantiate the store:
-  ActiveJob::Status.store = ActiveSupport::Cache::FileStore.new('tmp/jobcache')
-  # ActiveJob::Status.store = ActiveSupport::Cache::RedisCacheStore.new(url: ENV['REDIS_CACHE_URL'])
-end
+#if Rails.cache.is_a?(ActiveSupport::Cache::NullStore)
+#  # Use an alternative cache store:
+#  # ActiveJob::Status.store = :file_store
+#  # ActiveJob::Status.store = :mem_cache_store
+#  # ActiveJob::Status.store = :redis_cache_store
+#  #ActiveJob::Status.store = :file_store
+#    
+#  # Avoid using cache store that are not shared by processes (ex: memory_store).
+#
+#  # The `store=` method doesn't handle multiple arguments like Rails.
+#  # If you need to pass argument, you should instantiate the store:
+#  ActiveJob::Status.store = ActiveSupport::Cache::FileStore.new('tmp/jobcache')
+#  # ActiveJob::Status.store = ActiveSupport::Cache::RedisCacheStore.new(url: ENV['REDIS_CACHE_URL'])
+#end
