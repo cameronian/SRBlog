9c8f4edde9522921e08d08112dab3b612024a6d2
Ian <cameronian0@protonmail.com>
2021-07-08 20:43:04 +0800

    Load config path

--- config/initializers/srblog.rb
+++ config/initializers/srblog.rb
@@ -1,10 +1,17 @@
 
+require 'yaml'
 
 module SRBlog
-  Config = {
-    source_url: ['/mnt/Vault/08.Dev/01.Workspaces/packblog/packblog'],
-    published_url: File.join(Rails.root,'weblog')
-  }
+  ConfigPath = File.join(Rails.root,"srblog.yml")
+  if File.exist?(ConfigPath)
+    Config = YAML.load(File.read(ConfigPath), symbolize_names: true)
+  else
+    Config = { }
+    Config[:source_url] = []
+  end
+
+  Config[:published_url] = File.join(Rails.root, 'weblog') 
+
 end
 
 require 'fileutils'
